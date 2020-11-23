f5beb70c3533b37df784460447d82cc310beb93a
Dayane <dayaninhaes18@gmail.com>
2020-11-23 03:47:59 -0300

    Alterações

--- obj/Release/netcoreapp3.1/ProvaTecnica.AssemblyInfo.cs
+++ obj/Release/netcoreapp3.1/ProvaTecnica.AssemblyInfo.cs
@@ -11,6 +11,7 @@
 using System;
 using System.Reflection;
 
+[assembly: Microsoft.Extensions.Configuration.UserSecrets.UserSecretsIdAttribute("68249726-2553-4fa2-a9cf-b7282e4bd20a")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProvaTecnica")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
