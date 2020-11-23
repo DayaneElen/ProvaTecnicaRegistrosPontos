4a94b75e795430564dbc2e7cdc61e6f150fd3ac0
Dayane <dayaninhaes18@gmail.com>
2020-11-23 02:01:19 -0300

    Alterações

--- SQL/dbo.RegistrosPontos.sql
+++ SQL/dbo.RegistrosPontos.sql
@@ -2,7 +2,7 @@
 (
     [Id] INT NOT NULL identity,
     [Data] DATE NULL,
-    [Hora] DATETIME NULL,
+    [Hora] TIME(7) NULL,
     [NomeUsuario] NVARCHAR (50) NULL,
     [Tipo] NCHAR (10) NULL,
     PRIMARY KEY CLUSTERED ([Id] ASC)
